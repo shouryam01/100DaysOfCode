1568d809c63af7910d6d523f9ed4640024e9d5bf
Shouryam Sankritya <57291774+shouryam01@users.noreply.github.com>
2023-07-19 13:59:46 +0530

    Add files via upload

---
+++ Day-1/leetcode1137.cpp
@@ -0,0 +1,24 @@
+// Nth Tribonacci Series 
+
+class Solution {
+public:
+    int tribonacci(int n) {
+        if(n==0)
+        return 0;
+
+        if(n==1 || n==2)
+        return 1;
+
+        long long t0 = 0, t1 = 1, t2 = 1;
+        long long ans = 0;
+
+        for(int i=3; i<=n; i++){
+            ans = t0+t1+t2;
+            t0 = t1;
+            t1 = t2;
+            t2 = ans;
+        }
+        
+        return ans;
+    }
+};
