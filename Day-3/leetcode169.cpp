a1e77d38e982565cafed0acc8995ba2ab83d3951
Shouryam Sankritya <57291774+shouryam01@users.noreply.github.com>
2023-07-21 14:54:21 +0530

    Add files via upload

---
+++ Day-3/leetcode169.cpp
@@ -0,0 +1,23 @@
+// Majority Elements in an Array
+
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        
+      int n = nums.size();
+        unordered_map<int, int>mp;
+        
+        for(int i=0; i<n; i++){
+            mp[nums[i]]++;
+        }
+        n = n/2;
+        
+        for(auto x: mp){
+            if(x.second > n){
+                return x.first;
+            }
+        }
+        
+        return 0;
+    }
+};
