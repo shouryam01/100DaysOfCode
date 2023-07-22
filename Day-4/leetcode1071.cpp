3582a5358744aa1f7f87dc872a64daddcb3c88f5
Shouryam Sankritya <57291774+shouryam01@users.noreply.github.com>
2023-07-22 12:06:02 +0530

    Add files via upload

---
+++ Day-4/leetcode1071.cpp
@@ -0,0 +1,13 @@
+// GCD of strings:-)
+
+class Solution {
+public:
+    string gcdOfStrings(string str1, string str2) {
+        
+        if(str1 + str2 != str2 + str1)
+            return "";
+        
+        return str1.substr(0, gcd(size(str1), size(str2)));
+        
+    }
+};
